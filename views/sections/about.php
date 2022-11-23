0feac3abbaa3be0ee0e7487bfcefa6d70fb430d4
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2022-11-23 22:59:26 +0200

    update

--- views/sections/about.php
+++ views/sections/about.php
@@ -8,17 +8,19 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6">
-                <div class="right-side ml-3">
-                    <div class="title">
-                        <h1 class="bold text-left">ABOUT US</h1>
+            <div class="col-lg-6 p-0">
+                <div class="bg">
+                    <div class="right-side ml-3">
+                        <div class="title">
+                            <h1 class="bold text-left">ABOUT US</h1>
+                        </div>
+                        <div class="desc">
+                            <p class="text-left">
+                                The highly-trained technicians have many years of experience with all laptop, notebook, macbook and desktop computer repairs. We pride our company on honesty and the good relationships forged through customer satisfaction. We service all makes and models of MAC laptops, Imac, PC laptops, notebooks and most desktops: Dell, Sony, IBM, HP, Acer, Apple, etc.
+                            </p>
+                        </div>
+                        <a href="../../views/pages/about.php" class="btn btn-outline-success text-left">View More</a>
                     </div>
-                    <div class="desc">
-                        <p class="text-left">
-                            The highly-trained technicians have many years of experience with all laptop, notebook, macbook and desktop computer repairs. We pride our company on honesty and the good relationships forged through customer satisfaction. We service all makes and models of MAC laptops, Imac, PC laptops, notebooks and most desktops: Dell, Sony, IBM, HP, Acer, Apple, etc.
-                        </p>
-                    </div>
-                    <a href="about.php" class="btn btn-outline-success text-left">View More</a>
                 </div>
             </div>
         </div>
