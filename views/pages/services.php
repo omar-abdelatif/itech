c939ffc6bda68b314a7171190ff218961db78f28
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2022-11-24 00:00:54 +0200

    update

---
+++ views/pages/services.php
@@ -0,0 +1,6 @@
+<?php
+include "../inc/header.php";
+include "../inc/nav.php";
+?>
+<div class="banner"></div>
+<?php include "../inc/footer.php"; ?>
--- views/sections/services.php
+++ views/sections/services.php
@@ -1,3 +1,10 @@
 <section class="features">
-    <h1>Services Section</h1>
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4"></div>
+        </div>
+        <a href="../../views/pages/services.php" class="more btn btn-primary">View More</a>
+    </div>
 </section>
