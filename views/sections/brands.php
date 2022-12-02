3cd5dbee3c90dd57dd472bb10f9e90d84d8b5bd6
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2022-12-02 22:00:33 +0200

    update

--- views/sections/brands.php
+++ views/sections/brands.php
@@ -8,17 +8,17 @@
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
                     <div class="slider-img mb-4 text-center">
-                        <img src="https://via.placeholder.com/150" alt="rev-1">
+                        <img src="../../assets/imgs/images/brands/1-2-dell-logo-3d-white-png.png" alt="rev-1">
                     </div>
                 </div>
                 <div class="swiper-slide">
                     <div class="slider-img mb-4 text-center">
-                        <img src="https://via.placeholder.com/150" alt="rev-2">
+                        <img src="../../assets/imgs/images/brands/1024px-HP_logo_2012.svg.png" alt="rev-2">
                     </div>
                 </div>
                 <div class="swiper-slide">
                     <div class="slider-img mb-4 text-center">
-                        <img src="https://via.placeholder.com/150" alt="rev-3">
+                        <img src="../../assets/imgs/images/brands/HUAWEI WHITE.png" alt="rev-3">
                     </div>
                 </div>
             </div>
