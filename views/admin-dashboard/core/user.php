dee440f579b4c156504080ce9dcbb0c85014d079
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2022-12-25 17:14:15 +0200

    update

--- views/admin-dashboard/core/user.php
+++ views/admin-dashboard/core/user.php
@@ -10,3 +10,44 @@ function insertUser($name, $email, $password)
         return false;
     }
 }
+function selectUser($email, $password)
+{
+    $connection = connection();
+    $query = "SELECT * FROM `users` WHERE `email` = '$email' AND `password` = '$password'";
+    $res = mysqli_query($connection, $query);
+    return mysqli_fetch_assoc($res);
+}
+function deleteUser($id)
+{
+    $connection = connection();
+    $query = "DELETE FROM `users` WHERE `id` = $id";
+    mysqli_query($connection, $query);
+    return mysqli_affected_rows($connection);
+}
+function updateUser($id, $name, $email, $password, $img)
+{
+    $connection = connection();
+    if (!empty($img)) {
+        $extra = ", `img` = '$img'";
+    } else {
+        $extra = "";
+    }
+    $query = "UPDATE `users` SET `name` = '$name', `email` = '$email', `password` = '$password' $extra WHERE `id` = $id";
+    mysqli_query($connection, $query);
+    return mysqli_affected_rows($connection);
+}
+function users()
+{
+    $connection = connection();
+    $query = "SELECT * FROM `users`";
+    $res = mysqli_query($connection, $query);
+    return mysqli_fetch_all($res, MYSQLI_ASSOC);
+}
+function getUserById($id)
+{
+    $connection = connection();
+    $selectsql = "SELECT * FROM `users` WHERE `id` = $id";
+    $rsSelect = mysqli_query($connection, $selectsql);
+    $getRow = mysqli_fetch_assoc($rsSelect);
+    return $getRow;
+}
