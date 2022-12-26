b44d499b4e424edaefe2b543d34fedbcb1d35cc8
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2022-12-26 14:11:39 +0200

    update

--- views/admin-dashboard/core/register.php
+++ views/admin-dashboard/core/register.php
@@ -1,8 +1,4 @@
 <?php
-//! Include Auth
-// if (!isset($_SESSION['admin'])) {
-//     header("location: ../../pages/error.php");
-// }
 //! Include Connection
 include "../config/database.php";
 //! Include User
@@ -16,6 +12,26 @@ if (isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
     $cpass = $_POST['re-pass'];
+    if (empty($name)) {
+        $errors[] = "Name Is Required";
+    } elseif (strlen($name) < 3 || strlen($name) > 10) {
+        $errors[] = "Name Should Be Between 3 and 10";
+    }
+    if (empty($password)) {
+        $errors[] = "Password Is Required";
+    } elseif (strlen($password) < 3) {
+        $errors[] = "Password Should Be Greater Than 3 Char..";
+    }
+    if (empty($email)) {
+        $errors[] = "Email Is Required";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Email Is Not Valid";
+    } elseif (emailExistence($email)) {
+        $errors[] = "Email Already Exist";
+    }
+    if ($cpass != $password) {
+        $errors[] = "Password Doesn't Match";
+    }
     if (empty($errors)) {
         insertUser($name, $email, $password);
         $_SESSION['success'] = "User Inserted Successfully";
@@ -23,7 +39,7 @@ if (isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
         die;
     } else {
         $_SESSION['errors'] = $errors;
-        header("location: signup.php");
+        header("location: ../signup.php");
         die;
     }
 }
