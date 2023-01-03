38dcaa2c8f1fe37b6e7a8ec98e5b8217e2c6117c
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-01-03 21:26:20 +0200

    update

--- views/admin-dashboard/core/products/editproduct.php
+++ views/admin-dashboard/core/products/editproduct.php
@@ -2,3 +2,24 @@
 //! Open Seesion
 session_start();
 //! Include Auth
+if (!isset($_SESSION['login'])) {
+    header("location: ../../../pages/error.php");
+}
+//! Include Connection
+include "../../config/database.php";
+//! Include Products Functions
+include "products.php";
+//! Functions
+if (isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = $_POST['id'];
+    $product_name = $_POST['name'];
+    $product_price = $_POST['price'];
+    $avatar = $_FILES['img']['name'];
+    $tmp_name = $_FILES['img']['tmp_name'];
+    $path = '../../../../assets/imgs/images/products/';
+    $location = $path . $avatar;
+    uploadImage($tmp_name, $location);
+    updateProduct($id, $product_name, $product_price, $avatar);
+    header("location: ../../views/pages/products.php");
+    die;
+}
