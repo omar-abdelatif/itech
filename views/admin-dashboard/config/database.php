7c5c187143cabd9df8b950487149752157dee899
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-01-15 16:36:25 +0200

    update

--- views/admin-dashboard/config/database.php
+++ views/admin-dashboard/config/database.php
@@ -1,4 +1,8 @@
 <?php
+// function connection(){
+//     return mysqli_connect("localhost", "root", "", "fixit");
+// }
+//! Online Database
 function connection(){
-    return mysqli_connect("localhost", "root", "", "fixit");
+    return mysqli_connect("sql200.epizy.com", "epiz_33056117", "GJAnLYUpCf", "epiz_33056117_itech");
 }
